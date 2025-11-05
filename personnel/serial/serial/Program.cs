8ce4bcd6261656df12181c5258046e3378d35a6e
Bastien <po77qxd@eduvaud.ch>
2025-11-05 11:31:56 +0100

    feat(serial): partie 1

---
+++ personnel/serial/serial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace serial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character character = new Character() { FirstName = "prenom", LastName = "nom", Description = "description", PlayedBy = null };
+
+            string json = JsonSerializer.Serialize(character);
+            File.WriteAllText("data.json", json);
+        }
+    }
+
+    public class Character
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public Actor PlayedBy { get; set; }
+    }
+
+    public class Actor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+        public bool IsAlive { get; set; }
+    }
+}
